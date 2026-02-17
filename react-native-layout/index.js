0c7b9f561c2ccf9053a632070841f995d44be68c
Czarina Fombuena <czarinafombuena2019@gmail.com>
2026-02-17 22:49:12 +0800

    Initial Login Screen Design

--- react-native-layout/index.js
+++ react-native-layout/index.js
@@ -1,11 +1,9 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-import Up from './screens/Login/Up';
-import Up from './screens/Signup/Up';
+import LoginScreen from './screens/Login.js';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(Up);
-
+registerRootComponent(LoginScreen);
