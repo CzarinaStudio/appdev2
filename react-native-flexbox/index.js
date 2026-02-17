a4ae7589bd4f9035d40e22d8ac36bb2c53e234b7
Czarina Fombuena <czarinafombuena2019@gmail.com>
2026-02-17 22:09:06 +0800

    Done with flex wrap section

--- react-native-flexbox/index.js
+++ react-native-flexbox/index.js
@@ -1,10 +1,10 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-import AlignContentLayout from './components/style.js';
+import FlexWrapLayout from './components/style.js';
 
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(AlignContentLayout);
+registerRootComponent(FlexWrapLayout);
