6f0285fc57417f0b846ba8cc1b1ab0a05e120d45
Czarina Fombuena <czarinafombuena2019@gmail.com>
2026-02-17 22:06:50 +0800

    Done with align self section

--- react-native-flexbox/components/style.js
+++ react-native-flexbox/components/style.js
@@ -1,30 +1,60 @@
 import {useState} from 'react';
 import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
 
-const AlignItemsLayout = () => {
-  const [alignItems, setAlignItems] = useState('stretch');
+const AlignSelfLayout = () => {
+  const [alignSelf, setAlignSelf] = useState('stretch');
 
   return (
     <PreviewLayout
-      label="alignItems"
-      selectedValue={alignItems}
+      label="alignSelf"
+      selectedValue={alignSelf}
       values={['stretch', 'flex-start', 'flex-end', 'center', 'baseline']}
-      setSelectedValue={setAlignItems}>
-      <View style={[styles.box, {backgroundColor: 'powderblue'}]} />
-      <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
+      setSelectedValue={setAlignSelf}>
       <View
         style={[
           styles.box,
           {
-            backgroundColor: 'steelblue',
+            alignSelf,
             width: 'auto',
             minWidth: 50,
+            backgroundColor: 'powderblue',
           },
         ]}
       />
+      <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
+      <View style={[styles.box, {backgroundColor: 'steelblue'}]} />
     </PreviewLayout>
   );
 };
+
+const PreviewLayout = ({
+  label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
+}) => (
+  <View style={{padding: 10, flex: 1}}>
+    <Text style={styles.label}>{label}</Text>
+    <View style={styles.row}>
+      {values.map(value => (
+        <TouchableOpacity
+          key={value}
+          onPress={() => setSelectedValue(value)}
+          style={[styles.button, selectedValue === value && styles.selected]}>
+          <Text
+            style={[
+              styles.buttonLabel,
+              selectedValue === value && styles.selectedLabel,
+            ]}>
+            {value}
+          </Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+    <View style={styles.container}>{children}</View>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -71,33 +101,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default AlignItemsLayout;
-
-const PreviewLayout = ({
-  label,
-  children,
-  values,
-  selectedValue,
-  setSelectedValue,
-}) => (
-  <View style={{padding: 10, flex: 1}}>
-    <Text style={styles.label}>{label}</Text>
-    <View style={styles.row}>
-      {values.map(value => (
-        <TouchableOpacity
-          key={value}
-          onPress={() => setSelectedValue(value)}
-          style={[styles.button, selectedValue === value && styles.selected]}>
-          <Text
-            style={[
-              styles.buttonLabel,
-              selectedValue === value && styles.selectedLabel,
-            ]}>
-            {value}
-          </Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-    <View style={[styles.container, {[label]: selectedValue}]}>{children}</View>
-  </View>
-);
+export default AlignSelfLayout;
--- react-native-flexbox/index.js
+++ react-native-flexbox/index.js
@@ -1,10 +1,10 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
-import AlignItemsLayout from './components/style.js';
+import AlignSelfLayout from './components/style.js';
 
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
-registerRootComponent(AlignItemsLayout);
+registerRootComponent(AlignSelfLayout);
